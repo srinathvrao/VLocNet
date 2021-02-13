3843085d1a32755cf19e83cdacc74be7eb4c66b5
Srinath V <srinathv1001@gmail.com>
2021-02-13 17:05:55 +0530

    Modified VLocNet

--- code/loadingdata.py
+++ code/loadingdata.py
@@ -15,72 +15,102 @@ import torch.nn as nn
 
 from qmath import calc_vo_relative, calc_vo_relative_logq
 
-from vlocnet import SiameseNetwork as SiamNN
+from models import VLocNet
 
 from customdataset import scene, Rescale, RandomCrop, ToTensor
+from IPython.display import HTML, display
+import time
+print("Finished imports.")
+
+def progress(value, c, max=100):
+    return HTML("""
+        <progress
+            value='{value}',
+            max='{max}',
+            style='width: 40%'
+        >
+            {value}
+        </progress>
+        {c}
+    """.format(value=value,c=c, max=max))
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+print("Device selected:",device)
 fire_trainset = scene(
-					scene_data='fire/',
-					train=True,
-					transform=transforms.Compose([
-						Rescale(256),
-						RandomCrop(224),
-						ToTensor()
+                    scene_data='fire/',
+                    train=True,
+                    transform=transforms.Compose([
+                        Rescale(256),
+                        RandomCrop(224),
+                        ToTensor()
                    ]))
-
-
-trainloader = DataLoader(fire_trainset, batch_size=2, num_workers=2)
+print("Dataset declared.")
+batch_size = 20
+trainloader = DataLoader(fire_trainset, batch_size=batch_size, num_workers=2)
+print("Dataloader declared.")
 import math
 c=0
 
-siamnn = SiamNN().double().cuda()
-
+vlocn = VLocNet().double().cuda()
+print("Vlocnet initialized.")
 z = len(trainloader)
 
-loss = nn.MSELoss()
-for param in siamnn.parameters():
-	param.requires_grad = True
-optimizer = optim.Adam(siamnn.parameters(), lr=1e-4)
-running_loss = 0.0
-mo = nn.Sequential(*list(siamnn.children())[0]).cuda()
-print(mo)
-exit(0)
-print("starting training...")
+mseloss = nn.MSELoss()
+for param in vlocn.parameters():
+    param.requires_grad = True
+
+optimizer = optim.Adam(vlocn.parameters(), lr=1e-4)
+l = len(trainloader)*batch_size
+print("Starting Training")
+
 for epoch in range(120):
-	c=0
-	for i in trainloader: # goes 2 images and 2 positions at a time, through the dataset.
-		img_minus1 = i['image'][0].double().numpy()
-		img_1 = i['image'][1].double().numpy()
-		# output = siamnn.forward(x,xminus1)
-		xminus1 = torch.from_numpy(np.array([img_minus1])).double()
-		# p_minus1 = np.array([i['pose'][0].double().numpy()[:-1]])
-		p_minus1 = np.array([i['pose'][0].double().numpy()])
+    c=0
+    # out = display(progress(0, l), display_id=True)
+    # printProgressBar(0, l, prefix = 'Epoch '+str(epoch+1)+':', suffix = 'Complete', length = 50)
+    for batch in trainloader: # loads batch_size number of images and poses
+        y_pred = torch.empty(0,7).cuda()
+        y_true = torch.empty(0,7).cuda()
 
-		x = torch.from_numpy(np.array([img_1])).double()
-		# p = np.array([i['pose'][1].double().numpy()[:-1]])
-		p = np.array([i['pose'][1].double().numpy()])
-		# torch.from_numpy()
-		# print(calc_vo_relative_logq(torch.from_numpy(p_minus1),torch.from_numpy(p)))
-		xminus1 = xminus1.to(device)
-		x = x.to(device)
-		output = siamnn(x, xminus1)
-		l2loss = torch.sqrt(loss(output,calc_vo_relative(torch.from_numpy(p_minus1),torch.from_numpy(p)).to(device)))
-		running_loss += l2loss.item()
-		if (c+1)%32 == 0:
-			print('[%d, %5d] loss: %.3f' %(epoch + 1, c + 1, running_loss / 16))
-			optimizer.zero_grad()
-			l2loss.backward()
-			optimizer.step()
-			running_loss = 0.0
-		c+=1
+        pose_pred = torch.empty(0,7).cuda()
+        pose_true = torch.empty(0,7).cuda()
 
-	if epoch % 10 == 0:
-		PATH = "models/epoch"+str(epoch+1)+".pth"
-		torch.save(siamnn, PATH)
-		# model = torch.load(PATH)
-		# model.eval()
+        for i in range(0,batch_size-1): # considering 2 samples (i, i+1) at a time in a batch
+            img_minus1 = batch['image'][i].unsqueeze(0).to(device)
+            img_1 = batch['image'][i+1].unsqueeze(0).to(device)
+            p_minus1 = batch['pose'][i].unsqueeze(0)
+            p_1 = batch['pose'][i+1].unsqueeze(0)
+
+            task1_op, task2_op = vlocn(img_1,img_minus1)
+            y_pred = torch.cat((y_pred, task1_op.unsqueeze(0)), 0)
+            
+            actual_output = calc_vo_relative(p_minus1,p_1).cuda()
+            y_true = torch.cat((y_true, actual_output), 0)
+
+            pose_pred = torch.cat((pose_pred, task2_op.unsqueeze(0)), 0)
+            pose_true = torch.cat((pose_true, p_1.cuda()), 0)
+
+            del img_minus1, img_1, p_minus1, p_1, task1_op, task2_op, actual_output
+
+        loss = mseloss(y_pred, y_true) + mseloss(pose_pred,pose_true)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        del y_pred, y_true, pose_pred, pose_true
+    
+        c+=batch_size
+        st = "Epoch "+str(epoch+1)+" "+str(c)+"/"+str(l)+" Loss: "+str(loss.item())
+        print(st)
+        # out.update(progress(c, st, l))
+        # printProgressBar(c, l, prefix = 'Epoch '+str(epoch+1)+':', suffix = 'vo_loss: '+'{:.4f}'.format(loss)+' gpose_loss: '+'{:.4f}'.format(gposeloss)  , length = 50)
+        
+    if (epoch+1) % 5 == 0:
+        print("Saving Checkpoint..")
+        PATH = "models/epoch"+str(epoch+1)+".pth"
+        torch.save(vlocn, PATH)
+    
+        # model = torch.load(PATH)
+        # model.eval()
 
 '''
 n1 = calc_vo_relative(torch.from_numpy(p_minus1),torch.from_numpy(p)).numpy().flatten()
